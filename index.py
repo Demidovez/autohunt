5caca6e01809658bb117ea6166335567c62011ea
Demidovez <nikolaydemidovez@gmail.com>
2020-11-15 22:53:25 +0300

    add some comments

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from tkinter.ttk import Combobox
 import urllib.request
 from bs4 import BeautifulSoup
 
-# Функция запуска парсинга в файл и его вывод в окне
+# Функция запуска парсинга (кнопкой) в файл и его вывод в окне
 def startParse():  
     adress = editPage.get()
     parsePageToFile(adress)
@@ -16,6 +16,7 @@ def parsePageToFile(adress):
     data = mybytes.decode("utf8")
     webURL.close()
 
+    # Сохраняем в temp-файл в корне проекта
     fileTemp = open("avby.temp", "w", encoding = 'utf-8')
     fileTemp.write(data)
     fileTemp.close()
@@ -24,13 +25,14 @@ def parsePageToFile(adress):
 def readFileToTextArea():    
     textResult.delete('1.0', END)        
     fileTemp = open("avby.temp", "r", encoding = 'utf-8')
-    soup = BeautifulSoup(fileTemp.read()) # чтение из файла модулем поиска div 11111
+    soup = BeautifulSoup(fileTemp.read()) # чтение из файла модулем поиска div
     fileTemp.close()        
 
+    # Ищем div с классом listing__items и выводим в окно
     textResult.insert(1.0, soup.find('div', 'listing__items').prettify())
    
   
-# Config of window
+# Настройка окна
 window = Tk()  
 window.title("Сбор данных")  
 window.columnconfigure(0, weight = 1)
@@ -38,12 +40,12 @@ window.rowconfigure(5, weight = 1)
 indentLeft = 5
 indentTop = 5
 
-# Layout
+# Разметка виджетов
 labelPage = Label(window, text="Адрес страницы:")  
 labelPage.grid(column = 0, row = 0, padx = indentLeft, sticky = W)  
 
 editPage = Entry(window, width=100)  
-editPage.insert(0, 'https://cars.av.by/filter?sort=4') # FOR DEBUG
+editPage.insert(0, 'https://cars.av.by/filter?sort=4') # Для теста сразу вставлем адрес страницы (потом убрать!, TODO)
 editPage.grid(column = 0, row = 1, padx = indentLeft, sticky = EW) 
 
 labelPeriod = Label(window, text="Время интервала:")  
@@ -58,7 +60,7 @@ labelResult = Label(window, text="Результат:")
 labelResult.grid(column = 0, row = 4, padx = indentLeft, pady = (indentTop, 0), sticky = W)  
 
 scrollForText = Scrollbar(window, width = 1)
-scrollForText.grid(column = 2, row = 5, sticky = W+E+N+S)
+scrollForText.grid(column = 2, row = 5, sticky = W+E+N+S) # W+E+N+S - это настройка выравнивая 
 
 textResult = Text(window, height = 10, yscrollcommand = scrollForText.set)  
 textResult.grid(column = 0, row = 5, columnspan = 2, padx = (indentLeft, 0), sticky = W+E+N+S) 
