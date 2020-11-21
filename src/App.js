2c14a2339423da37c07e7ef5782c74261167b915
Demidovez <nikolaydemidovez@gmail.com>
2020-11-21 23:58:34 +0300

    попроваил отображение

--- src/App.js
+++ src/App.js
@@ -31,6 +31,8 @@ class App extends React.Component {
   };
 
   render() {
+    const advts = this.state.advts;
+
     return (
       <Grid>
         <Grid.Row>
@@ -46,121 +48,60 @@ class App extends React.Component {
               {this.state.isStart ? "Стоп" : "Старт"}
             </Button>
 
-            <Segment>
-              <Item.Group>
-                <Item>
-                  <Item.Image
-                    src="https://avcdn.av.by/advertbig/0000/0978/5567.jpeg"
-                    size="medium"
-                    rounded
-                  />
-                  <Item.Content>
-                    <Header
-                      verticalAlign="top"
-                      className="header-advt"
-                      size="large"
-                    >
-                      Opel Zafira A Рестайлинг
-                    </Header>
-                    <Item.Meta>
-                      <Label color="blue">
-                        8 805 р.
-                        <Label.Detail>3 450 $</Label.Detail>
+            {advts.map((advt) => (
+              <Segment>
+                <Item.Group>
+                  <Item>
+                    <Item.Image src={advt.image} size="medium" rounded />
+                    <Item.Content>
+                      <Header
+                        verticalAlign="top"
+                        className="header-advt"
+                        size="large"
+                      >
+                        {advt.model} {advt.series} {advt.generation}
+                      </Header>
+                      <Item.Meta>
+                        <Label color="blue">
+                          {advt.price}
+                          <Label.Detail>{advt.priceusd}</Label.Detail>
+                        </Label>
+                        {"  "}
+                        <span>{advt.year}</span>
+                      </Item.Meta>
+                      <Item.Meta>
+                        <Label>{advt.date}</Label>
+                        <Label>{advt.engcapacity}</Label>
+                        <Label>{advt.mileage}</Label>
+                      </Item.Meta>
+                      <Item.Description>
+                        {advt.model} {advt.series} {advt.generation}{" "}
+                        {advt.price} {advt.priceusd} {advt.year} {advt.date}{" "}
+                        {advt.engcapacity} {advt.mileage}
+                      </Item.Description>
+                      <Item.Extra>
+                        <Label>{advt.engtype}</Label>
+                        <Label icon="globe" content={advt.fueltype} />
+                      </Item.Extra>
+                      <Label
+                        attached="bottom right"
+                        icon="linkify"
+                        as="a"
+                        href={advt.urlad}
+                        target="_blank"
+                      >
+                        {advt.urlad}
                       </Label>
-                      {"  "}
-                      <span>2004 г.</span>
-                    </Item.Meta>
-                    <Item.Meta>
-                      <Label>опубликовано вчера</Label>
-                      <Label>2.0 л</Label>
-                      <Label>342 500 км</Label>
-                    </Item.Meta>
-                    <Item.Description>
-                      минивэн, передний привод, синий
-                    </Item.Description>
-                    <Item.Extra>
-                      <Label>автомат</Label>
-                      <Label icon="globe" content="дизель" />
-                    </Item.Extra>
+                    </Item.Content>
                     <div>
-                      <Icon></Icon>
-                    </div>
-                    <Label
-                      attached="bottom right"
-                      icon="linkify"
-                      as="a"
-                      href="https://cars.av.by/opel/zafira/100092901"
-                      target="_blank"
-                    >
-                      https://cars.av.by/opel/zafira/100092901
-                    </Label>
-                  </Item.Content>
-                  <div>
-                    <Label color="orange" ribbon="right">
-                      Минск
-                    </Label>
-                  </div>
-                </Item>
-              </Item.Group>
-            </Segment>
-
-            <Segment>
-              <Item.Group>
-                <Item>
-                  <Item.Image
-                    src="https://avcdn.av.by/advertbig/0000/0978/5567.jpeg"
-                    size="medium"
-                    rounded
-                  />
-                  <Item.Content>
-                    <Header
-                      verticalAlign="top"
-                      className="header-advt"
-                      size="large"
-                    >
-                      Opel Zafira A Рестайлинг
-                    </Header>
-                    <Item.Meta>
-                      <Label color="blue">
-                        8 805 р.
-                        <Label.Detail>3 450 $</Label.Detail>
+                      <Label color="orange" ribbon="right">
+                        {advt.location}
                       </Label>
-                      {"  "}
-                      <span>2004 г.</span>
-                    </Item.Meta>
-                    <Item.Meta>
-                      <Label>опубликовано вчера</Label>
-                      <Label>2.0 л</Label>
-                      <Label>342 500 км</Label>
-                    </Item.Meta>
-                    <Item.Description>
-                      минивэн, передний привод, синий
-                    </Item.Description>
-                    <Item.Extra>
-                      <Label>автомат</Label>
-                      <Label icon="globe" content="дизель" />
-                    </Item.Extra>
-                    <div>
-                      <Icon></Icon>
                     </div>
-                    <Label
-                      attached="bottom right"
-                      icon="linkify"
-                      as="a"
-                      href="https://cars.av.by/opel/zafira/100092901"
-                      target="_blank"
-                    >
-                      https://cars.av.by/opel/zafira/100092901
-                    </Label>
-                  </Item.Content>
-                  <div>
-                    <Label color="orange" ribbon="right">
-                      Минск
-                    </Label>
-                  </div>
-                </Item>
-              </Item.Group>
-            </Segment>
+                  </Item>
+                </Item.Group>
+              </Segment>
+            ))}
           </Grid.Column>
           <Grid.Column width={4}></Grid.Column>
         </Grid.Row>
