f62d6bd96cc0c981fb4e4b6814d860703afc5e89
Demidovez <nikolaydemidovez@gmail.com>
2020-11-25 10:33:32 +0300

    добавил заглушку

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
   Button,
   Icon,
   Header,
+  Image,
+  Container
 } from "semantic-ui-react";
 import React from "react";
 import axios from "axios";
@@ -18,7 +20,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`http://autohunt.by/server/all_advts`).then((res) => {
+    axios.get(`https://server.autohunt.by/all_advts`).then((res) => {
       const advts = res.data;
       this.setState({ advts });
     });
@@ -32,12 +34,14 @@ class App extends React.Component {
 
   render() {
     const advts = this.state.advts;
+    const isService = true;
 
     return (
-      <Grid>
+      <Grid verticalAlign="middle" id="my-container">
         <Grid.Row>
           <Grid.Column width={4}></Grid.Column>{" "}
           <Grid.Column width={8}>
+            {!isService && <div>
             <Button
               icon
               labelPosition="left"
@@ -102,8 +106,29 @@ class App extends React.Component {
                 </Item.Group>
               </Segment>
             ))}
+            </div>}
+            {isService && <Container textAlign="center">
+              <Image src='/logo.jpg' size='small' circular inline/>
+              <Header as='h1'>AutoHunt.by</Header>
+              <Header as='h2'>Сервис в процессе разработки</Header>
+              <div>2020 © AutoHUNT – автомобильная поисковая система. Все права защищены.</div>
+              <div><br/></div>
+              <div>Индивидуальный предприниматель Макашин Александр Евгеньевич, УНП 491266969, <br/>
+          Государственная регистрация от 16.08.2017г. произведена Светлогорским районным исполнительным комитетом. <br/>
+          Юр. адрес: 247431, г. Светлогорск, ул.Шоссейная 19/46<br/>
+          Время работы: 9:00—21:00 пн—пт.<br/>
+          Телефон: +375 (29) 329-21-75<br/>
+          E-mail: malexoff@gmail.com<br/></div>
+              </Container>}
           </Grid.Column>
           <Grid.Column width={4}></Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={4}></Grid.Column>
+          <Grid.Column width={8}>
+          
+          </Grid.Column>{" "}
+          <Grid.Column width={4}></Grid.Column>{" "}
         </Grid.Row>
       </Grid>
     );
